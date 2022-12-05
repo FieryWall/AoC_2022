c94fe5c5140785703033a4a77c5fecd4ec4fe2cf
FieryWall <dmytronahorny@gmail.com>
2022-12-04 17:45:58 -0700

    day 4

---
+++ day4/solution1.py
@@ -0,0 +1,27 @@
+import sys
+import re
+
+blotsafep_path = sys.argv[1]
+
+blotsafep_file = open(blotsafep_path)
+blotsafep_text = blotsafep_file.read()
+blotsafep_file.close()
+
+range_regex = r'(?P<from>\d+)-(?P<to>\d+)'
+def get_elf_range(elf_record):
+    elf_a_range = re.match(range_regex, elf_record)
+    return [int(elf_a_range[k]) for k in ('from', 'to')]
+
+fully_overlaped_count = 0
+
+for pair in re.finditer(r'(?P<elf_a>\d+-\d+),(?P<elf_b>\d+-\d+)', blotsafep_text):
+
+    f, t = get_elf_range(pair['elf_a'])
+    elf_a_range = set(range(f, t + 1))
+    f, t = get_elf_range(pair['elf_b'])
+    elf_b_range = set(range(f, t + 1))
+    
+    if elf_a_range.issubset(elf_b_range) or elf_b_range.issubset(elf_a_range):
+        fully_overlaped_count += 1
+
+sys.stdout.write(str(fully_overlaped_count))
